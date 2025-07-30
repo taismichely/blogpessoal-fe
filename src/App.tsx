6c0bc5b6bacb4884eba54de09521b029460cb6cb
taismichely <michely2207@gmail.com>
2025-07-29 22:22:21 -0300

    fix: Address correction

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
       <Navbar/>
       <div className='min-h-[80vh]'>
         <Routes>
-          {/* <Route path='/' element={<Home/>}/>
-          <Route path='/home' element={<Home/>}/> */}
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/login" element={<Login />} />
         </Routes>
