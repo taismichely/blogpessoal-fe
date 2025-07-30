b1dc3fb765e67a1b577f54dd20a6db469403a248
taismichely <michely2207@gmail.com>
2025-07-30 13:23:30 -0300

    feat: add AuthProvider component in the App to have access to Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/Home';
-import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import { AuthProvider } from './contexts/AuthContext';
 import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import './App.css'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navbar/>
-      <div className='min-h-[80vh]'>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-      <Footer/>
-    </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar/>
+            <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/home' element={<Home/>}/>
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </div>
+          <Footer/>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
